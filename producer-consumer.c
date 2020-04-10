ae264e6a0cb4aa2ee64165a21dae591b1388321e
Wilson <wilsonzhang6@gmail.com>
2020-04-09 18:50:28 -0700

    edit

--- producer-consumer.c
+++ producer-consumer.c
@@ -10,15 +10,16 @@
 #include <pthread.h>
 
 int main(int argc, char* argv[]){
-  int socket;
-  struct sockaddr_un socket_addr;
-  memset(&socket_addr, '\0', sizeof(socket_addr));
+  int sock;
+  struct sockaddr_un sock_addr;
+  memset(&sock_addr, '\0', sizeof(sock_addr));
 
   int producer = -1;
   char msg[1024];
   int queue_size = -1;
-  int socket = -1;
+  int socket_ipc = -1;
   int e = 0;
+  char *queue;
   for(int i = 1; i < argc; i++){
     if(argv[i] == "-p"){
       producer = 1;
@@ -27,27 +28,67 @@ int main(int argc, char* argv[]){
       producer = 0;
     }
     else if(argv[i] == "-m"){
-      msg = argv[i+1];
+      strcpy(msg, argv[i+1]);
     }
     else if(argv[i] == "-q"){
-      queue_size = argv[i+1];
+      queue_size = atoi(argv[i+1]);
     }
     else if(argv[i] == "-u"){ //unix socket
-      socket = 1;
+      socket_ipc = 1;
     }
     else if(argv[i] == "-s"){ //shared memory
-      socket = 0;
+      socket_ipc = 0;
     }
     else if(argv[i] == "-e"){
       e = 1;
     }
   }
-  if(producer == -1 || queue_size == -1 || socket == -1){
+  if(producer == -1 || queue_size == -1 || socket_ipc == -1 || msg[0] == "\n"){
     printf("error: not enough arguments specified\n");
     exit(0);
   }
+  queue = (char *) malloc(1024*queue_size*sizeof(char));
+  //for i = 0 to r(for j = 0 to c(*(queue + i*c + j) = msg;))
 
   if(producer){
-    
+    if(socket_ipc){
+      int client_socket;
+      struct sockaddr_un client_addr;
+      memset(&client_addr, '\0', sizeof(client_addr));
+      sock = socket(AF_UNIX, SOCK_STREAM, 0);
+      if(sock == -1){
+        fprintf(stderr, "error %i creating producer socket\n", errno);
+      }
+      sock_addr.sun_family = AF_UNIX;
+      strcpy(sock_addr.sun_path, "sock");
+      if(bind(sock, (struct sockaddr*) &sock_addr, sizeof(sock_addr)) < 0){
+        fprintf(stderr, "error %i bind producer\n", errno);
+      }
+      if(listen(sock, 1) < 0){
+        fprintf(stderr, "error %i listen producer\n", errno);
+      }
+      int client_addr_size = sizeof(client_addr);
+      if(((client_socket = accept(sock, (struct sockaddr *)&client_addr, &client_addr_size)) < 0)){
+        fprintf(stderr, "error %i accept producer\n", errno);
+      }
+      while(true){
+        
+      }
+    }
+  }
+
+  //consumer
+  else{
+    if(socket_ipc){
+      sock = socket(AF_UNIX, SOCK_STREAM, 0);
+      if(sock == -1){
+        fprintf(stderr, "error %i creating producer socket\n", errno);
+      }
+      sock_addr.sun_family = AF_UNIX;
+      strcpy(sock_addr.sun_path, "sock");
+      if(connect(sock, (const struct sockaddr*) &sock_addr, sizeof(sock_addr)) < 0){
+        fprintf(stderr, "error %i connect consumer\n", errno);
+      }
+    }
   }
 }
